f7539fca1eae7c378a81678814f00e6417656f0f
appDIbetu <dipakshah401@gmail.com>
2022-09-26 23:04:33 +0545

    initial

--- app/config.py
+++ app/config.py
@@ -1,7 +1,7 @@
 import asyncio
 
 # env Variable
-KAFKA_BOOTSTRAP_SERVERS= "168.119.119.105"
+KAFKA_BOOTSTRAP_SERVERS= "168.119.119.105:9094"
 KAFKA_TOPIC="myTopic"
 KAFKA_CONSUMER_GROUP="group-id"
 loop = asyncio.get_event_loop()
