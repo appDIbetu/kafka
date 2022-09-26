24b1ae428e3e826724032440cfd97f81c05b6cd8
appDIbetu <dipakshah401@gmail.com>
2022-09-27 00:51:07 +0545

    initial

--- app/router.py
+++ app/router.py
@@ -24,8 +24,8 @@ async def consume():
     consumer = AIOKafkaConsumer(KAFKA_TOPIC, loop=loop,
                                 bootstrap_servers=KAFKA_BOOTSTRAP_SERVERS, group_id=KAFKA_CONSUMER_GROUP)
     await consumer.start()
-    try:
-        async for msg in consumer:
-            print(f'Consumer msg: {msg}')
-    finally:
-        await consumer.stop()
+    #try:
+     #   async for msg in consumer:
+      #      print(f'Consumer msg: {msg}')
+    #finally:
+     #   await consumer.stop()
