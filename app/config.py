75c8816deea1093236184773bb880040ef8734f8
appDIbetu <dipakshah401@gmail.com>
2022-09-27 13:20:09 +0545

    testing a

--- app/config.py
+++
@@ -1,7 +0,0 @@
-import asyncio
-
-# env Variable
-KAFKA_BOOTSTRAP_SERVERS= "168.119.119.105:9094"
-KAFKA_TOPIC="myTopic"
-KAFKA_CONSUMER_GROUP="group-id"
-loop = asyncio.get_event_loop()
--- app/main.py
+++ app/main.py
@@ -1,12 +1,76 @@
-from fastapi import FastAPI
-import router
 import asyncio
+import json
 
+from aiokafka import AIOKafkaConsumer, AIOKafkaProducer
+from fastapi import FastAPI
+
+from pydantic import BaseModel, StrictStr
+
+
+class ProducerResponse(BaseModel):
+    name: StrictStr
+    message_id: StrictStr
+    topic: StrictStr
+    timestamp: StrictStr = ""
+
+
+class ProducerMessage(BaseModel):
+    name: StrictStr
+    message_id: StrictStr = ""
+    timestamp: StrictStr = ""
 app = FastAPI()
 
-@app.get('/')
-async def Home():
-    return "welcome home"
+KAFKA_INSTANCE = "168.119.119.105:9094"
 
-app.include_router(router.route)
-asyncio.create_task(router.consume())
+
+loop = asyncio.get_event_loop()
+
+
+aioproducer = AIOKafkaProducer(loop=loop, bootstrap_servers=KAFKA_INSTANCE)
+
+consumer = AIOKafkaConsumer("testtopic", bootstrap_servers=KAFKA_INSTANCE, loop=loop)
+
+
+async def consume():
+    await consumer.start()
+    try:
+        async for msg in consumer:
+            print(
+                "consumed: ",
+                msg.topic,
+                msg.partition,
+                msg.offset,
+                msg.key,
+                msg.value,
+                msg.timestamp,
+            )
+
+    finally:
+        await consumer.stop()
+
+
+@app.on_event("startup")
+async def startup_event():
+    await aioproducer.start()
+    loop.create_task(consume())
+
+
+@app.on_event("shutdown")
+async def shutdown_event():
+    await aioproducer.stop()
+    await consumer.stop()
+
+
+@app.get("/")
+def read_root():
+    return {"Ping": "Pong"}
+
+@app.post("/producer/{topicname}")
+async def kafka_produce(msg: ProducerMessage, topicname: str):
+
+    await aioproducer.send(topicname, json.dumps(msg.dict()).encode("ascii"))
+    response = ProducerResponse(
+        name=msg.name, message_id=msg.message_id, topic=topicname
+    )
+
+    return response
